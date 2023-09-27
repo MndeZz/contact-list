3a615812cb685f394314414852c3b1d4cd3ce20a
MndeZz <111843756+MndeZz@users.noreply.github.com>
2023-09-26 20:37:49 -0600

    subo la parte de modificar un contacto

--- index.js
+++ index.js
@@ -66,4 +66,43 @@ function eliminarContacto(){
 
 }
 
+//modificar un contacto indice-modificar
 
+function modificar_contacto() {
+    let indice_modificar = document.getElementById("indice-modificar")
+    let indice_modificar_s = indice_modificar.value
+    let indice_modificar_s_s = parseInt(indice_modificar_s)
+
+    let id_modificar = document.getElementById("indice-modificar")
+    let id_modificar_s = id_modificar.value
+    let id_modificar_s_s = parseInt(id_modificar_s)
+
+    let nombre_modificar = document.getElementById("nombre-modificar")
+    let nombre_modificar_s = nombre_modificar.value
+
+    let apellido_modificar = document.getElementById("apellido-modificar")
+    let apellido_modificar_s = apellido_modificar.value
+
+    let telefono_modificar = document.getElementById("telefono-modificar")
+    let telefono_modificar_s = telefono_modificar.value
+
+    let ciudad_modificar = document.getElementById("ciudad-modificar")
+    let ciudad_modificar_s = ciudad_modificar.value
+
+    let direccion_modificar = document.getElementById("direccion-modificar")
+    let direccion_modificar_s = direccion_modificar.value
+//
+    let contacto_modificar = ListaDeContactos[indice_modificar_s_s]
+
+    contacto_modificar.id = id_modificar_s_s
+    contacto_modificar.nombre = nombre_modificar_s
+    contacto_modificar.apellido = apellido_modificar_s
+    contacto_modificar.telefono = telefono_modificar_s
+    contacto_modificar.ciudad = ciudad_modificar_s
+    contacto_modificar.direccion = direccion_modificar_s
+
+    console.clear();
+    for (var objeto of ListaDeContactos) {
+        console.log(objeto);
+    }
+}
